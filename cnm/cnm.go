431e7b0eb90f8cdd2923a84aad2d3c88026dbf19
Onur Filiz <onur.filiz@microsoft.com>
2016-10-11 15:47:02 -0700

    Added command line options for CNM plugin

--- cnm/cnm.go
+++ cnm/cnm.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/Azure/azure-container-networking/common"
@@ -20,33 +19,86 @@ import (
 const (
 	// Plugin name.
 	name = "azure"
+
+	// Command line options.
+	OptEnvironment = "environment"
+	OptLogLevel    = "log-level"
+	OptLogTarget   = "log-target"
+	OptVersion     = "version"
 )
 
 // Version is populated by make during build.
 var version string
 
-// Prints description and usage information.
-func printHelp() {
-	fmt.Printf("Azure container networking plugin\n")
-	fmt.Printf("Version %v\n\n", version)
-	fmt.Printf("Usage: aqua [OPTIONS]\n\n")
-	fmt.Printf("Options:\n")
-	fmt.Printf("  -e, --environment={azure|mas}         Set the operating environment.\n")
-	fmt.Printf("  -l, --log-level={info|debug}          Set the logging level.\n")
-	fmt.Printf("  -t, --log-target={syslog|stderr}      Set the logging target.\n")
-	fmt.Printf("  -?, --help                            Print usage and version information.\n\n")
+// Command line arguments for CNM plugin.
+var args = common.ArgumentList{
+	{
+		Name:         OptEnvironment,
+		Shorthand:    "e",
+		Description:  "Set the operating environment",
+		Type:         "string",
+		DefaultValue: "azure",
+		ValueMap: map[string]interface{}{
+			"azure": 0,
+			"mas":   0,
+		},
+	},
+	{
+		Name:         OptLogLevel,
+		Shorthand:    "l",
+		Description:  "Set the logging level",
+		Type:         "int",
+		DefaultValue: "info",
+		ValueMap: map[string]interface{}{
+			"info":  log.LevelInfo,
+			"debug": log.LevelDebug,
+		},
+	},
+	{
+		Name:         OptLogTarget,
+		Shorthand:    "t",
+		Description:  "Set the logging target",
+		Type:         "int",
+		DefaultValue: "stderr",
+		ValueMap: map[string]interface{}{
+			"syslog": log.TargetSyslog,
+			"stderr": log.TargetStderr,
+		},
+	},
+	{
+		Name:         OptVersion,
+		Shorthand:    "v",
+		Description:  "Print version information",
+		Type:         "bool",
+		DefaultValue: false,
+	},
 }
 
+// Prints description and version information.
+func printVersion() {
+	fmt.Printf("Azure CNM (libnetwork) plugin\n")
+	fmt.Printf("Version %v\n", version)
+}
+
+// Main is the entry point for CNM plugin.
 func main() {
 	var netPlugin network.NetPlugin
 	var ipamPlugin ipam.IpamPlugin
 	var config common.PluginConfig
 	var err error
 
-	// Set defaults.
-	environment := common.OptEnvironmentAzure
-	logLevel := log.LevelInfo
-	logTarget := log.TargetStderr
+	// Initialize and parse command line arguments.
+	common.ParseArgs(&args, printVersion)
+
+	environment := common.GetArg(OptEnvironment).(string)
+	logLevel := common.GetArg(OptLogLevel).(int)
+	logTarget := common.GetArg(OptLogTarget).(int)
+	vers := common.GetArg(OptVersion).(bool)
+
+	if vers {
+		printVersion()
+		os.Exit(0)
+	}
 
 	// Initialize plugin common configuration.
 	config.Name = name
@@ -64,64 +116,6 @@ func main() {
 	if err != nil {
 		fmt.Printf("Failed to create IPAM plugin %v\n", err)
 		return
-	}
-
-	// Parse command line arguments.
-	args := os.Args[1:]
-
-	for _, arg := range args {
-		if !strings.HasPrefix(arg, "-") {
-			// Process commands.
-			switch arg {
-
-			default:
-				fmt.Printf("Invalid command: %s\n", arg)
-				printHelp()
-				return
-			}
-		} else {
-			// Process options of format "--key=value".
-			arg = strings.TrimLeft(arg, "-")
-			opt := strings.SplitN(arg, "=", 2)
-			if len(opt) == 1 {
-				opt = append(opt, "")
-			}
-
-			switch opt[0] {
-			case common.OptEnvironmentKey, common.OptEnvironmentKeyShort:
-				environment = opt[1]
-
-			case common.OptLogLevelKey, common.OptLogLevelKeyShort:
-				switch opt[1] {
-				case common.OptLogLevelInfo:
-					logLevel = log.LevelInfo
-				case common.OptLogLevelDebug:
-					logLevel = log.LevelDebug
-				default:
-					fmt.Printf("Invalid option: %v\nSee --help.\n", arg)
-					return
-				}
-
-			case common.OptLogTargetKey, common.OptLogTargetKeyShort:
-				switch opt[1] {
-				case common.OptLogTargetStderr:
-					logTarget = log.TargetStderr
-				case common.OptLogTargetSyslog:
-					logTarget = log.TargetSyslog
-				default:
-					fmt.Printf("Invalid option: %v\nSee --help.\n", arg)
-					return
-				}
-
-			case common.OptHelpKey, common.OptHelpKeyShort:
-				printHelp()
-				return
-
-			default:
-				fmt.Printf("Invalid option: %v\nSee --help.\n", arg)
-				return
-			}
-		}
 	}
 
 	// Create a channel to receive unhandled errors from the plugins.
