c892141057e4c9fa549bf469b35c4496c9eaa0e7
Onur Filiz <onur.filiz@microsoft.com>
2016-11-18 17:09:22 -0800

    Added support for changing network namespaces

---
+++ network/namespace.go
@@ -0,0 +1,100 @@
+// Copyright Microsoft Corp.
+// All rights reserved.
+
+package network
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"golang.org/x/sys/unix"
+)
+
+// Namespace represents a network namespace.
+type Namespace struct {
+	file   *os.File
+	prevNs *Namespace
+}
+
+// OpenNamespace creates a new namespace object for the given netns path.
+func OpenNamespace(nsPath string) (*Namespace, error) {
+	fd, err := os.Open(nsPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Namespace{file: fd}, nil
+}
+
+// GetCurrentThreadNamespace returns the caller thread's current namespace.
+func GetCurrentThreadNamespace() (*Namespace, error) {
+	nsPath := fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), unix.Gettid())
+	return OpenNamespace(nsPath)
+}
+
+// Close releases the resources associated with the namespace object.
+func (ns *Namespace) Close() error {
+	if ns.file == nil {
+		return nil
+	}
+
+	err := ns.file.Close()
+	if err != nil {
+		return fmt.Errorf("Failed to close namespace %v, err:%v", ns.file.Name(), err)
+	}
+
+	ns.file = nil
+
+	return nil
+}
+
+// GetFd returns the file descriptor of the namespace.
+func (ns *Namespace) GetFd() uintptr {
+	return ns.file.Fd()
+}
+
+// Set sets the current namespace.
+func (ns *Namespace) set() error {
+	_, _, err := unix.Syscall(unix.SYS_SETNS, ns.file.Fd(), uintptr(unix.CLONE_NEWNET), 0)
+	if err != 0 {
+		return fmt.Errorf("Failed to set namespace %v, err:%v", ns.file.Name(), err)
+	}
+
+	return nil
+}
+
+// Enter puts the caller thread inside the namespace.
+func (ns *Namespace) Enter() error {
+	var err error
+
+	ns.prevNs, err = GetCurrentThreadNamespace()
+	if err != nil {
+		return err
+	}
+
+	runtime.LockOSThread()
+
+	err = ns.set()
+	if err != nil {
+		runtime.UnlockOSThread()
+		return err
+	}
+
+	return nil
+}
+
+// Exit puts the caller thread to its previous namespace.
+func (ns *Namespace) Exit() error {
+	err := ns.prevNs.set()
+	if err != nil {
+		return err
+	}
+
+	ns.prevNs.Close()
+	ns.prevNs = nil
+
+	runtime.UnlockOSThread()
+
+	return nil
+}
