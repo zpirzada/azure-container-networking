e39a72110342aac945f4f4f3eb65db7d093b5b1c
Onur Filiz <onur.filiz@microsoft.com>
2017-02-01 21:11:53 -0800

    Added CNI IPAM plugin test skeleton

---
+++ cni/ipam/ipam_test.go
@@ -0,0 +1,94 @@
+// Copyright Microsoft Corp.
+// All rights reserved.
+
+package ipam
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/Azure/azure-container-networking/common"
+)
+
+var plugin *ipamPlugin
+
+var ipamQueryUrl = "localhost:42424"
+var ipamQueryResponse = "" +
+	"<Interfaces>" +
+	"	<Interface MacAddress=\"*\" IsPrimary=\"true\">" +
+	"		<IPSubnet Prefix=\"10.0.0.0/16\">" +
+	"			<IPAddress Address=\"10.0.0.4\" IsPrimary=\"true\"/>" +
+	"			<IPAddress Address=\"10.0.0.5\" IsPrimary=\"false\"/>" +
+	"			<IPAddress Address=\"10.0.0.6\" IsPrimary=\"false\"/>" +
+	"		</IPSubnet>" +
+	"	</Interface>" +
+	"</Interfaces>"
+
+var localAsId string
+var poolId1 string
+var address1 string
+
+// Wraps the test run with plugin setup and teardown.
+func TestMain(m *testing.M) {
+	var config common.PluginConfig
+
+	// Create a fake local agent to handle requests from IPAM plugin.
+	testAgent, err := common.NewListener("tcp", ipamQueryUrl)
+	if err != nil {
+		fmt.Printf("Failed to create agent, err:%v.\n", err)
+		return
+	}
+	testAgent.AddHandler("/", handleIpamQuery)
+
+	err = testAgent.Start(make(chan error, 1))
+	if err != nil {
+		fmt.Printf("Failed to start agent, err:%v.\n", err)
+		return
+	}
+
+	// Create the plugin.
+	plugin, err = NewPlugin(&config)
+	if err != nil {
+		fmt.Printf("Failed to create IPAM plugin, err:%v.\n", err)
+		return
+	}
+
+	// Configure test mode.
+	plugin.Name = "test"
+	plugin.SetOption(common.OptEnvironment, common.OptEnvironmentAzure)
+	plugin.SetOption(common.OptIpamQueryUrl, "http://"+ipamQueryUrl)
+
+	// Start the plugin.
+	err = plugin.Start(&config)
+	if err != nil {
+		fmt.Printf("Failed to start IPAM plugin, err:%v.\n", err)
+		return
+	}
+
+	// Run tests.
+	exitCode := m.Run()
+
+	// Cleanup.
+	plugin.Stop()
+	testAgent.Stop()
+
+	os.Exit(exitCode)
+}
+
+// Handles queries from IPAM source.
+func handleIpamQuery(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(ipamQueryResponse))
+}
+
+//
+// CNI IPAM API compliance tests
+// https://github.com/containernetworking/cni/blob/master/SPEC.md
+//
+
+func TestAddSuccess(t *testing.T) {
+}
+
+func TestDelSuccess(t *testing.T) {
+}
