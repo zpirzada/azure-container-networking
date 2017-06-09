80c5bd8a559bf63bf990ad8bae14515b7ef1a5e3
tamilmani1989 <tamilmani1989@gmail.com>
2017-06-09 16:14:03 -0700

    Add address IDs to CNM IPAM plugin (#63)

--- cnm/ipam/ipam_test.go
+++ cnm/ipam/ipam_test.go
@@ -12,9 +12,11 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/Azure/azure-container-networking/common"
+	"github.com/Azure/azure-container-networking/ipam"
 )
 
 var plugin IpamPlugin
@@ -312,5 +314,94 @@ func TestGetPoolInfo(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("GetPoolInfo response is invalid %+v", resp)
+	}
+}
+
+// Utility function to request address from IPAM.
+func reqAddrInternal(payload *requestAddressRequest) (string, error) {
+	var body bytes.Buffer
+	var resp requestAddressResponse
+	json.NewEncoder(&body).Encode(payload)
+
+	req, err := http.NewRequest(http.MethodGet, requestAddressPath, &body)
+	if err != nil {
+		return "", err
+	}
+
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, req)
+
+	err = decodeResponse(w, &resp)
+
+	if err != nil {
+		return "", err
+	}
+	return resp.Address, nil
+}
+
+// Utility function to release address from IPAM.
+func releaseAddrInternal(payload *releaseAddressRequest) error {
+	var body bytes.Buffer
+	var resp releaseAddressResponse
+
+	json.NewEncoder(&body).Encode(payload)
+
+	req, err := http.NewRequest(http.MethodGet, releaseAddressPath, &body)
+	if err != nil {
+		return err
+	}
+
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, req)
+
+	err = decodeResponse(w, &resp)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Tests IpamDriver.RequestAddress with reservation id.
+func TestRequestAddressWithID(t *testing.T) {
+	var ipList [2]string
+
+	for i := 0; i < 2; i++ {
+
+		payload := &requestAddressRequest{
+			PoolID:  poolId1,
+			Address: "",
+			Options: make(map[string]string),
+		}
+
+		payload.Options[ipam.OptAddressID] = "reserve" + strconv.Itoa(i)
+
+		addr1, err := reqAddrInternal(payload)
+		if err != nil {
+			t.Errorf("RequestAddress with id  response is invalid %+v", err)
+		}
+
+		addr2, err := reqAddrInternal(payload)
+		if err != nil {
+			t.Errorf("RequestAddress with id  response is invalid %+v", err)
+		}
+
+		if addr1 != addr2 {
+			t.Errorf("RequestAddress with id %+v doesn't match with retrieved addr %+v ", addr1, addr2)
+		}
+
+		address, _, _ := net.ParseCIDR(addr1)
+		ipList[i] = address.String()
+	}
+
+	for i := 0; i < 2; i++ {
+		payload := &releaseAddressRequest{
+			PoolID:  poolId1,
+			Address: ipList[i],
+		}
+		err := releaseAddrInternal(payload)
+		if err != nil {
+			t.Errorf("ReleaseAddress response is invalid %+v", err)
+		}
 	}
 }
