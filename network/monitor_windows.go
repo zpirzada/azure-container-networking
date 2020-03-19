0b62fc40c7d00f7df89877413a2800c86150d531
Paul Johnston <35265851+pjohnst5@users.noreply.github.com>
2020-03-18 17:57:34 -0700

    Fixing windows test failure (#531)
    * Testing windows change
    
    * Found error

--- network/monitor_windows.go
+++ network/monitor_windows.go
@@ -1,3 +1,7 @@
-func (nm *networkManager) monitorNetworkState() error {
+package network
+
+import cnms "github.com/Azure/azure-container-networking/cnms/cnmspackage"
+
+func (nm *networkManager) monitorNetworkState(networkMonitor *cnms.NetworkMonitor) error {
 	return nil
 }
