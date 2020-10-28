a3b3f511a650128331824b043f8f4640cf5cd2ef
Mathew Merrick <matmerr@microsoft.com>
2020-10-28 16:02:19 -0700

    another nil check

--- cni/network/invoker_azure.go
+++ cni/network/invoker_azure.go
@@ -80,11 +80,12 @@ func (invoker *AzureIPAMInvoker) Add(nwCfg *cni.NetworkConfig, subnetPrefix *net
 }
 
 func (invoker *AzureIPAMInvoker) Delete(address *net.IPNet, nwCfg *cni.NetworkConfig, options map[string]interface{}) error {
-	var err error
 
-	if address == nil {
-		if err = invoker.plugin.DelegateDel(nwCfg.Ipam.Type, nwCfg); err != nil {
-			log.Printf("Network not found, attempted to release address with error:  %v", err)
+	if nwCfg == nil {
+		return invoker.plugin.Errorf("nil nwCfg passed to CNI ADD, stack: %+v", string(debug.Stack()))
+	} else if address == nil {
+		if err := invoker.plugin.DelegateDel(nwCfg.Ipam.Type, nwCfg); err != nil {
+			return invoker.plugin.Errorf("Network not found, attempted to release address with error:  %v", err)
 		}
 	} else if len(address.IP.To4()) == 4 {
 
@@ -115,7 +116,7 @@ func (invoker *AzureIPAMInvoker) Delete(address *net.IPNet, nwCfg *cni.NetworkCo
 			return invoker.plugin.Errorf("Failed to release ipv6 address: %v", err)
 		}
 	} else {
-		err = fmt.Errorf("Address is incorrect, not valid IPv4 or IPv6")
+		return invoker.plugin.Errorf("Address is incorrect, not valid IPv4 or IPv6, stack: %+v", string(debug.Stack()))
 	}
 
 	return nil
