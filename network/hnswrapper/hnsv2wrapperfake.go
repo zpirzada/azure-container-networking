8879728064ad1d7dec4863a89818718013a7c3b0
Vamsi Kalapala <vkalapa@ncsu.edu>
2021-10-28 14:01:31 -0700

    [NPM] Expanding HNS fake usage with internal state

--- network/hnswrapper/hnsv2wrapperfake.go
+++ network/hnswrapper/hnsv2wrapperfake.go
@@ -7,13 +7,37 @@
 package hnswrapper
 
 import (
+	"encoding/json"
+	"reflect"
+	"sync"
+
 	"github.com/Microsoft/hcsshim/hcn"
 )
 
+type fakeHNSCache struct {
+	networks  map[string]hcn.HostComputeNetwork
+	endpoints map[string]hcn.HostComputeEndpoint
+}
+
 type Hnsv2wrapperFake struct {
+	cache fakeHNSCache
+	sync.Mutex
+}
+
+func NewHnsv2wrapperFake() *Hnsv2wrapperFake {
+	return &Hnsv2wrapperFake{
+		cache: fakeHNSCache{
+			networks:  map[string]hcn.HostComputeNetwork{},
+			endpoints: map[string]hcn.HostComputeEndpoint{},
+		},
+	}
 }
 
 func (f Hnsv2wrapperFake) CreateNetwork(network *hcn.HostComputeNetwork) (*hcn.HostComputeNetwork, error) {
+	f.Lock()
+	defer f.Unlock()
+
+	f.cache.networks[network.Name] = *network
 	return network, nil
 }
 
@@ -21,7 +45,54 @@ func (f Hnsv2wrapperFake) DeleteNetwork(network *hcn.HostComputeNetwork) error {
 	return nil
 }
 
-func (Hnsv2wrapperFake) ModifyNetworkSettings(network *hcn.HostComputeNetwork, request *hcn.ModifyNetworkSettingRequest) error {
+func (f Hnsv2wrapperFake) ModifyNetworkSettings(network *hcn.HostComputeNetwork, request *hcn.ModifyNetworkSettingRequest) error {
+	f.Lock()
+	defer f.Unlock()
+	switch request.RequestType {
+	case hcn.RequestTypeAdd:
+		var setPolSettings []hcn.NetworkPolicy
+		err := json.Unmarshal(request.Settings, &setPolSettings)
+		if err != nil {
+			return err
+		}
+		for _, setPolSetting := range setPolSettings {
+			if setPolSetting.Type == hcn.SetPolicy {
+				network.Policies = append(network.Policies, setPolSetting)
+			}
+		}
+	case hcn.RequestTypeRemove:
+		newtempPolicies := network.Policies
+		var setPolSettings []hcn.NetworkPolicy
+		err := json.Unmarshal(request.Settings, &setPolSettings)
+		if err != nil {
+			return err
+		}
+		for i, policy := range network.Policies {
+			for _, newPolicy := range setPolSettings {
+				if policy.Type != newPolicy.Type {
+					continue
+				}
+				if reflect.DeepEqual(policy.Settings, newPolicy.Settings) {
+					newtempPolicies = append(newtempPolicies[:i], newtempPolicies[i+1:]...)
+					break
+				}
+			}
+		}
+		network.Policies = newtempPolicies
+	case hcn.RequestTypeUpdate:
+		network.Policies = []hcn.NetworkPolicy{}
+		var setPolSettings []hcn.NetworkPolicy
+		err := json.Unmarshal(request.Settings, &setPolSettings)
+		if err != nil {
+			return err
+		}
+		for _, setPolSetting := range setPolSettings {
+			if setPolSetting.Type == hcn.SetPolicy {
+				network.Policies = append(network.Policies, setPolSetting)
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -33,25 +104,46 @@ func (Hnsv2wrapperFake) RemoveNetworkPolicy(network *hcn.HostComputeNetwork, net
 	return nil
 }
 
-func (Hnsv2wrapperFake) GetNetworkByName(networkName string) (*hcn.HostComputeNetwork, error) {
+func (f Hnsv2wrapperFake) GetNetworkByName(networkName string) (*hcn.HostComputeNetwork, error) {
+	f.Lock()
+	defer f.Unlock()
+	if network, ok := f.cache.networks[networkName]; ok {
+		return &network, nil
+	}
 	return &hcn.HostComputeNetwork{}, nil
 }
 
 func (f Hnsv2wrapperFake) GetNetworkByID(networkID string) (*hcn.HostComputeNetwork, error) {
-	network := &hcn.HostComputeNetwork{Id: networkID}
-	return network, nil
+	f.Lock()
+	defer f.Unlock()
+	for _, network := range f.cache.networks {
+		if network.Id == networkID {
+			return &network, nil
+		}
+	}
+	return &hcn.HostComputeNetwork{}, nil
 }
 
 func (f Hnsv2wrapperFake) GetEndpointByID(endpointID string) (*hcn.HostComputeEndpoint, error) {
-	endpoint := &hcn.HostComputeEndpoint{Id: endpointID}
+	f.Lock()
+	defer f.Unlock()
+	if ep, ok := f.cache.endpoints[endpointID]; ok {
+		return &ep, nil
+	}
+	return &hcn.HostComputeEndpoint{}, nil
+}
+
+func (f Hnsv2wrapperFake) CreateEndpoint(endpoint *hcn.HostComputeEndpoint) (*hcn.HostComputeEndpoint, error) {
+	f.Lock()
+	defer f.Unlock()
+	f.cache.endpoints[endpoint.Id] = *endpoint
 	return endpoint, nil
 }
 
-func (Hnsv2wrapperFake) CreateEndpoint(endpoint *hcn.HostComputeEndpoint) (*hcn.HostComputeEndpoint, error) {
-	return endpoint, nil
-}
-
-func (Hnsv2wrapperFake) DeleteEndpoint(endpoint *hcn.HostComputeEndpoint) error {
+func (f Hnsv2wrapperFake) DeleteEndpoint(endpoint *hcn.HostComputeEndpoint) error {
+	f.Lock()
+	defer f.Unlock()
+	delete(f.cache.endpoints, endpoint.Id)
 	return nil
 }
 
@@ -68,10 +160,52 @@ func (Hnsv2wrapperFake) RemoveNamespaceEndpoint(namespaceId string, endpointId s
 	return nil
 }
 
-func (Hnsv2wrapperFake) ListEndpointsOfNetwork(networkId string) ([]hcn.HostComputeEndpoint, error) {
-	return []hcn.HostComputeEndpoint{}, nil
+func (f Hnsv2wrapperFake) ListEndpointsOfNetwork(networkId string) ([]hcn.HostComputeEndpoint, error) {
+	f.Lock()
+	defer f.Unlock()
+	endpoints := make([]hcn.HostComputeEndpoint, 0)
+	for _, endpoint := range f.cache.endpoints {
+		if endpoint.HostComputeNetwork == networkId {
+			endpoints = append(endpoints, endpoint)
+		}
+	}
+	return endpoints, nil
 }
 
-func (Hnsv2wrapperFake) ApplyEndpointPolicy(endpoint *hcn.HostComputeEndpoint, requestType hcn.RequestType, endpointPolicy hcn.PolicyEndpointRequest) error {
+func (f Hnsv2wrapperFake) ApplyEndpointPolicy(endpoint *hcn.HostComputeEndpoint, requestType hcn.RequestType, endpointPolicy hcn.PolicyEndpointRequest) error {
+	f.Lock()
+	defer f.Unlock()
+	switch requestType {
+	case hcn.RequestTypeAdd:
+		for _, newPolicy := range endpointPolicy.Policies {
+			if newPolicy.Type != hcn.ACL {
+				continue
+			}
+			endpoint.Policies = append(endpoint.Policies, newPolicy)
+		}
+	case hcn.RequestTypeRemove:
+		newtempPolicies := endpoint.Policies
+		for i, policy := range endpoint.Policies {
+			for _, newPolicy := range endpointPolicy.Policies {
+				if policy.Type != newPolicy.Type {
+					continue
+				}
+				if reflect.DeepEqual(policy.Settings, newPolicy.Settings) {
+					newtempPolicies = append(newtempPolicies[:i], newtempPolicies[i+1:]...)
+					break
+				}
+			}
+		}
+		endpoint.Policies = newtempPolicies
+	case hcn.RequestTypeUpdate:
+		endpoint.Policies = make([]hcn.EndpointPolicy, 0)
+		for _, newPolicy := range endpointPolicy.Policies {
+			if newPolicy.Type != hcn.ACL {
+				continue
+			}
+			endpoint.Policies = append(endpoint.Policies, newPolicy)
+		}
+	}
+
 	return nil
 }
