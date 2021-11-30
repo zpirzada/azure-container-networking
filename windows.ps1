4658dfd39501d93dcdd7f012c4d42fd48b56996e
Mathew Merrick <matmerr@users.noreply.github.com>
2021-11-30 11:57:54 -0800

    retry ci (#1134)

--- windows.ps1
+++ windows.ps1
@@ -1,8 +1,27 @@
+# Retry({azure-npm-image $(tag)-windows-amd64})
+
+function Retry([Action]$action) {
+    $attempts = 3    
+    $sleepInSeconds = 5
+    do {
+        try {
+            $action.Invoke();
+            break;
+        }
+        catch [Exception] {
+            Write-Host $_.Exception.Message
+        }            
+        $attempts--
+        if ($attempts -gt 0) { 
+            sleep $sleepInSeconds 
+        }
+    } while ($attempts -gt 0)    
+}
 
 function azure-npm-image {
     $env:ACN_PACKAGE_PATH = "github.com/Azure/azure-container-networking"
     $env:NPM_AI_ID = "014c22bd-4107-459e-8475-67909e96edcb"
-    $env:NPM_AI_PATH="$env:ACN_PACKAGE_PATH/npm.aiMetadata"
+    $env:NPM_AI_PATH = "$env:ACN_PACKAGE_PATH/npm.aiMetadata"
 
     if ($null -eq $env:VERSION) { $env:VERSION = $args[0] } 
     docker build `
