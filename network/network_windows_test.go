2157d7ad2c20d49618ccbad3ac79c644176713bd
aegal <aegal@outlook.com>
2022-04-14 13:41:14 -0700

    Skip network creation if network already exists in HNS (#1300)
    * skip network creation if network already exists
    
    * address comments
    
    * address feedback

--- network/network_windows_test.go
+++ network/network_windows_test.go
@@ -11,6 +11,8 @@ import (
 	"testing"
 
 	"github.com/Azure/azure-container-networking/network/hnswrapper"
+
+	"github.com/Microsoft/hcsshim/hcn"
 )
 
 func TestNewAndDeleteNetworkImplHnsV2(t *testing.T) {
@@ -20,7 +22,7 @@ func TestNewAndDeleteNetworkImplHnsV2(t *testing.T) {
 
 	// this hnsv2 variable overwrites the package level variable in network
 	// we do this to avoid passing around os specific objects in platform agnostic code
-	hnsv2 = hnswrapper.Hnsv2wrapperFake{}
+	hnsv2 = hnswrapper.NewHnsv2wrapperFake()
 
 	nwInfo := &NetworkInfo{
 		Id:           "d3e97a83-ba4c-45d5-ba88-dc56757ece28",
@@ -41,6 +43,41 @@ func TestNewAndDeleteNetworkImplHnsV2(t *testing.T) {
 	}
 
 	err = nm.deleteNetworkImplHnsV2(network)
+
+	if err != nil {
+		fmt.Printf("+%v", err)
+		t.Fatal(err)
+	}
+}
+
+func TestSuccesfulNetworkCreationWhenAlreadyExists(t *testing.T) {
+	nm := &networkManager{
+		ExternalInterfaces: map[string]*externalInterface{},
+	}
+
+	// this hnsv2 variable overwrites the package level variable in network
+	// we do this to avoid passing around os specific objects in platform agnostic code
+	hnsv2 = hnswrapper.NewHnsv2wrapperFake()
+
+	network := &hcn.HostComputeNetwork{
+		Name: "azure-vlan1-172-28-1-0_24",
+	}
+
+	_, err := hnsv2.CreateNetwork(network)
+
+	// network name is derived from network info id
+	nwInfo := &NetworkInfo{
+		Id:           "azure-vlan1-172-28-1-0_24",
+		MasterIfName: "eth0",
+		Mode:         "bridge",
+	}
+
+	extInterface := &externalInterface{
+		Name:    "eth0",
+		Subnets: []string{"subnet1", "subnet2"},
+	}
+
+	_, err = nm.newNetworkImplHnsV2(nwInfo, extInterface)
 
 	if err != nil {
 		fmt.Printf("+%v", err)
