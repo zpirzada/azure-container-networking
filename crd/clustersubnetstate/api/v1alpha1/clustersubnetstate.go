79e0867c40169e6901389a163b2face63a589a5f
rsagasthya <agasthya.rahul1@gmail.com>
2022-07-21 17:18:05 -0700

    Creating new CRD for ClusterSubnetState. (#1465)
    * Creating new CRD for ClusterSubnetState.
    
    * Changes to the CRD definition.
    
    * Updated new ClusterSubnetState CRD. Generated YAML files.
    
    * Changed bool Status to Exhausted.
    
    * Changed bool Status to Exhausted.

---
+++ crd/clustersubnetstate/api/v1alpha1/clustersubnetstate.go
@@ -0,0 +1,49 @@
+//go:build !ignore_uncovered
+// +build !ignore_uncovered
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Important: Run "make" to regenerate code after modifying this file
+
+// +kubebuilder:object:root=true
+
+// ClusterSubnetState is the Schema for the ClusterSubnetState API
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Namespaced
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Timestamp",type=string,JSONPath=`.status.exhausted`
+// +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.spec.timestamp`
+type ClusterSubnetState struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterSubnetStateSpec   `json:"spec,omitempty"`
+	Status ClusterSubnetStateStatus `json:"status,omitempty"`
+}
+
+// ClusterSubnetStateSpec defines the desired state of ClusterSubnetState
+type ClusterSubnetStateSpec struct {
+	Timestamp string `json:"Timestamp"`
+}
+
+// ClusterSubnetStateStatus defines the observed state of ClusterSubnetState
+type ClusterSubnetStateStatus struct {
+	Exhausted bool `json:"Exhausted"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterSubnetStateList contains a list of ClusterSubnetState
+type ClusterSubnetStateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterSubnetState `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterSubnetState{}, &ClusterSubnetStateList{})
+}
---
+++ crd/clustersubnetstate/api/v1alpha1/groupversion_info.go
@@ -0,0 +1,23 @@
+//go:build !ignore_uncovered
+// +build !ignore_uncovered
+
+// Package v1alpha contains API Schema definitions for the acn v1alpha API group
+// +kubebuilder:object:generate=true
+// +groupName=acn.azure.com
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "acn.azure.com", Version: "v1alpha1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
