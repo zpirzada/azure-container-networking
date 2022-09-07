a2231dec4294cfc9c19c49c5a63b144d254e0532
Hunter Gregory <42728408+huntergregory@users.noreply.github.com>
2022-09-07 11:50:38 -0700

    test: [WIN-NPM] ignore a test case with an unsupported namedport (#1581)
    exclude update-policy tag which consists of 1 test with unsupported namedport

--- test/cyclonus/test-cyclonus-windows.sh
+++ test/cyclonus/test-cyclonus-windows.sh
@@ -8,7 +8,7 @@ curl -fsSL github.com/mattfenwick/cyclonus/releases/latest/download/cyclonus_lin
     --pod-creation-timeout-seconds=480 \
     --job-timeout-seconds=15 \
     --server-protocol=TCP,UDP \
-    --exclude sctp,named-port,ip-block-with-except,multi-peer,upstream-e2e,example,end-port,namespaces-by-default-label | tee cyclonus-$CLUSTER_NAME
+    --exclude sctp,named-port,ip-block-with-except,multi-peer,upstream-e2e,example,end-port,namespaces-by-default-label,update-policy | tee cyclonus-$CLUSTER_NAME
 
 rc=0
 cat cyclonus-$CLUSTER_NAME | grep "failed" > /dev/null 2>&1 || rc=$?
