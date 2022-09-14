3ce568fbe14fe0adb6aa4d1b03e5a0f907751ef1
Hunter Gregory <hunterlgregory@gmail.com>
2022-09-14 13:34:16 -0700

    eliminate effects of TTL for Windows flow table

--- test/cyclonus/test-cyclonus-windows.sh
+++ test/cyclonus/test-cyclonus-windows.sh
@@ -1,10 +1,13 @@
 curl -fsSL github.com/mattfenwick/cyclonus/releases/latest/download/cyclonus_linux_amd64.tar.gz | tar -zxv
+# As of 09/14, TTL for Windows flow table is up to 240 seconds.
+# Probes take at least 20 seconds (from logs), so with 7 retries, we will start our last try at least 140 seconds after perturbation.
+# Therefore, setting perturbation-wait-seconds=100 to avoid the effects of current TTL.
 ./cyclonus_linux_amd64/cyclonus generate \
     --noisy=true \
     --retries=7 \
     --ignore-loopback=true \
     --cleanup-namespaces=true \
-    --perturbation-wait-seconds=20 \
+    --perturbation-wait-seconds=80 \
     --pod-creation-timeout-seconds=480 \
     --job-timeout-seconds=15 \
     --server-protocol=TCP,UDP \
